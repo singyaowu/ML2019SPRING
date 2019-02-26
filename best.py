2658b107021755b0cca961452c31ff088d0ac789
singyaowu <r07922004@ntu.edu.tw>
2019-02-27 00:34:37 +0800

    modify GD

--- best.py
+++ best.py
@@ -5,25 +5,22 @@ if __name__ == "__main__":
     train_data = [ [] for i in range(18)]
 
     for i, line in enumerate(train_file.readlines()[1:]):
-        train_data[i%18] += [ float(data) if data.isnumeric() else 0.0 for data in line.split(',')[-24:] ]
+        train_data[i%18] += [ float(data) if data.isnumeric() else 0 for data in line.split(',')[-24:] ]
     train_file.close()
 
     # preprocessing
-    train_data = np.array(train_data).T # (numData, features)
+    train_data = np.array(train_data).T
     train_data = train_data.reshape(-1)
-    num_feature = 18
+    
     train_x, train_y = [], []
-    for i in range(0, len(train_data) - num_feature * 10, num_feature):
-        x = [1.0] + train_data[i: i+num_feature*9].tolist()
-        y = train_data[i + 18*9 + 9]
-        if y > 0:
-            train_x.append(x)
-            train_y.append(train_data[i + 18*9 + 9])
+    for i in range(0, len(train_data) - 180, 18):
+        train_x.append([1.0] + train_data[i: i+18*9].tolist() )
+        train_y.append(train_data[i + 18*9 + 9])
     train_x = np.array(train_x)
     train_y = np.array(train_y).T
     
     # training
-    w = np.array([-2.0] * len(train_x[0])).T#np.zeros(len(train_x[0]) ).T
+    w = np.array([-10.0] * len(train_x[0])).T#np.zeros(len(train_x[0]) ).T
     b = 0
     lr = 100.0
     iteration = 100000
