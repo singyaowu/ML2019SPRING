9d63f07e9f23ee2cab55a505095ce1f3edc35447
singyaowu <r07922004@ntu.edu.tw>
2019-02-27 01:14:52 +0800

    5.67033

--- simple.py
+++ simple.py
@@ -2,23 +2,41 @@ import numpy as np
 if __name__ == "__main__":    
     # read training data
     train_file = open('train.csv', 'r', encoding='big5')
-    train_data = [ [] for i in range(18)]
+    train_datas = [ [] for i in range(18)]
 
     for i, line in enumerate(train_file.readlines()[1:]):
-        train_data[i%18] += [ float(data) if data.isnumeric() else 0.0 for data in line.split(',')[-24:] ]
+        features = []
+        for data in line.split(',')[-24:]:
+            feature = 0
+            try: feature = float(data)
+            except ValueError: pass
+            features.append(feature)
+        train_datas[i%18] += features
+        #train_datas[i%18] += [ float(data) if data.isnumeric() else 0.0 for data in line.split(',')[-24:] ]
     train_file.close()
 
     # preprocessing
-    train_data = np.array(train_data).T # (numData, features)
-    train_data = train_data.reshape(-1)
+    train_datas = np.array(train_datas).T # (numData, features)
+    '''
+    for i in range(0, len(train_datas)-9):
+        train_data = train_datas[i: i+9]
+        test_x = []
+        
+        test_x = np.array(test_x).T.reshape(-1).tolist()
+        test_x = np.array([1] + test_x)
+        expect_y = test_x.dot(w)
+        id = test_data[0].split(',')[0]
+        output_file.write(id + ',' + str(expect_y) + '\n')
+    '''
+    train_datas = train_datas.reshape(-1)
     num_feature = 18
     train_x, train_y = [], []
-    for i in range(0, len(train_data) - num_feature * 10, num_feature):
-        x = [1.0] + train_data[i: i+num_feature*9].tolist()
-        y = train_data[i + 18*9 + 9]
+    for i in range(0, len(train_datas) - num_feature * 10, num_feature):
+        x = [1.0] + train_datas[i: i+num_feature*9].tolist()
+        y = train_datas[i + 18*9 + 9]
         if y > 0:
             train_x.append(x)
-            train_y.append(train_data[i + 18*9 + 9])
+            train_y.append(train_datas[i + 18*9 + 9])
     train_x = np.array(train_x)
     train_y = np.array(train_y).T
     
