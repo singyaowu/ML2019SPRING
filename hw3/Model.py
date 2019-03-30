d32c10a630a20857e8f481fadc9e7bbec215d50a
singyaowu <hoshiaki96@gmail.com>
2019-03-31 00:32:25 +0800

    simplified vGg

--- hw3/Model.py
+++ hw3/Model.py
@@ -13,70 +13,94 @@ from torchvision.transforms import *
 class MyCNN(nn.Module):
     def __init__(self):
         super(MyCNN, self).__init__()
-        self.conv1 = nn.Sequential(                     # input shape(1, 48, 48)
+        self.conv1 = nn.Sequential(           
             nn.Conv2d(in_channels=1,out_channels=64,
-                kernel_size=5,stride=1,padding=2,),     # output shape(16, 44, 44)
-            nn.LeakyReLU(0.05),
+                kernel_size=3,stride=1,padding=1,), 
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(64),
             nn.Dropout(0.25),
             nn.Conv2d(in_channels=64,out_channels=64,
-            kernel_size=5,stride=1,padding=2,),     # output shape(16, 44, 44)
-            nn.LeakyReLU(0.05),
+            kernel_size=5,stride=1,padding=2,),    
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(64),
             nn.MaxPool2d(kernel_size=2),                 # output shape(16, 22, 22)
             nn.BatchNorm2d(64),
-            nn.Dropout(0.3),
+            nn.Dropout(0.4),
 
-            nn.Conv2d(in_channels=64,out_channels=128,   # output shape(32, 18, 18)
+            nn.Conv2d(in_channels=64,out_channels=128,
                 kernel_size=3,stride=1,padding=1,),
-            nn.LeakyReLU(0.05),
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(128),
-            nn.Dropout(0.3),
-            nn.Conv2d(in_channels=128,out_channels=128,   # output shape(32, 18, 18)
+            nn.Dropout(0.4),
+            nn.Conv2d(in_channels=128,out_channels=128, 
                 kernel_size=3,stride=1,padding=1,),
-            nn.LeakyReLU(0.05),
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(128),
             nn.MaxPool2d(kernel_size=2),                 # output shape(32, 10, 10)
             nn.BatchNorm2d(128),
-            nn.Dropout(0.3),
+            nn.Dropout(0.4),
 
-            nn.Conv2d(in_channels=128,out_channels=256,   # output shape(32, 8, 8)
+            nn.Conv2d(in_channels=128,out_channels=256, 
                 kernel_size=3,stride=1,padding=1,),
             nn.LeakyReLU(0.05),
             nn.BatchNorm2d(256),
             nn.Dropout(0.4),
-            nn.Conv2d(in_channels=256,out_channels=256,   # output shape(32, 8, 8)
+            nn.Conv2d(in_channels=256,out_channels=256,   
                 kernel_size=3,stride=1,padding=1,),
-            nn.LeakyReLU(0.05),
+            nn.LeakyReLU(0.0005),
+            nn.BatchNorm2d(256),
+            nn.Conv2d(in_channels=256,out_channels=256,  
+                kernel_size=3,stride=1,padding=1,),
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(256),
             nn.MaxPool2d(kernel_size=2),                 # output shape(32, 4, 4)
             nn.BatchNorm2d(256),
-            nn.Dropout(0.4),
+            nn.Dropout(0.5),
 
-            nn.Conv2d(in_channels=256,out_channels=512,   # output shape(32, 8, 8)
+            nn.Conv2d(in_channels=256,out_channels=512,  
                 kernel_size=3,stride=1,padding=1,),
             nn.LeakyReLU(0.05),
+            nn.BatchNorm2d(512),
+            nn.Dropout(0.5),
+            #nn.Conv2d(in_channels=512,out_channels=512,   # output shape(32, 8, 8)
+            #    kernel_size=3,stride=1,padding=1,),
+            #nn.LeakyReLU(0.05),
+            #nn.BatchNorm2d(512),
+            nn.Conv2d(in_channels=512,out_channels=512,   # output shape(32, 8, 8)
+                kernel_size=3,stride=1,padding=1,),
+            nn.LeakyReLU(0.0005),
+            nn.BatchNorm2d(512),
+            nn.MaxPool2d(kernel_size=2),                 # output shape(32, 4, 4)
+            nn.BatchNorm2d(512),
+            nn.Dropout(0.4),
+
+            nn.Conv2d(in_channels=512,out_channels=512,   # output shape(32, 8, 8)
+                kernel_size=3,stride=1,padding=1,),
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(512),
             nn.Dropout(0.4),
             nn.Conv2d(in_channels=512,out_channels=512,   # output shape(32, 8, 8)
                 kernel_size=3,stride=1,padding=1,),
-            nn.LeakyReLU(0.05),
+            nn.LeakyReLU(0.0005),
             nn.BatchNorm2d(512),
+            #nn.Conv2d(in_channels=512,out_channels=512,   # output shape(32, 8, 8)
+            #    kernel_size=3,stride=1,padding=1,),
+            #nn.LeakyReLU(0.05),
+            #nn.BatchNorm2d(512),
             nn.MaxPool2d(kernel_size=2),                 # output shape(32, 4, 4)
             nn.BatchNorm2d(512),
-            nn.Dropout(0.4),
-
+            nn.Dropout(0.45),
         )
         self.fc = nn.Sequential(
-            nn.Linear(512 * 3 * 3, 512),
-            nn.BatchNorm1d(512),
+            nn.Linear(512 * 1 * 1, 1000),
+            nn.BatchNorm1d(1000),
             nn.Dropout(0.45),
-            nn.Linear(512, 7),
+            nn.Linear(1000, 7),
         )
-        self.out = nn.Softmax(dim=1)
+        #self.out = nn.Softmax(dim=1)
     def forward(self, x):
         x = self.conv1(x)
         x = x.view(x.size(0),-1)
         x = self.fc(x)
-        output = self.out(x)
-        return output
+        #output = self.out(x)
+        return x#output
--- hw3/train.py
+++ hw3/train.py
@@ -12,7 +12,7 @@ from torchvision import transforms as tf
 import Model
    
 # parameters
-EPOCH = 75
+EPOCH = 1000
 BATCH_SIZE = 256
 LEARNING_RATE = 0.001
 
@@ -40,15 +40,16 @@ def parse_csv(label_path):
     
     return raw_imgs, raw_y
 def augment_data(r_imgs, r_y):
-    f_imgs, f_y = flipped_data(r_imgs, r_y)
-    imgs = np.concatenate((r_imgs, f_imgs), axis=0)
+    #f_imgs, f_y = flipped_data(r_imgs, r_y)
+    #imgs = np.concatenate((r_imgs, f_imgs), axis=0)
     imgs = torch.tensor(imgs).type(torch.FloatTensor)
-    y = np.concatenate((r_y, f_y), axis=0)
+    #y = np.concatenate((r_y, f_y), axis=0)
     y = torch.tensor(y).type(torch.LongTensor)
     transform = tf.Compose([
             tf.ToPILImage(),
-            tf.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2, hue=0.2),            
-            tf.RandomRotation(18),
+            tf.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),            
+            tf.RandomHorizontalFlip(),
+            tf.RandomRotation(30),
             tf.RandomResizedCrop(48,scale=(0.85,1)),
             tf.ToTensor()
         ])
@@ -60,35 +61,36 @@ def augment_data(r_imgs, r_y):
         aug_imgs2[i, :, :, :] = transform(aug_imgs2[i])
     imgs = torch.cat((imgs,aug_imgs1, aug_imgs2), 0)
     y = torch.cat((y, y, y), 0)
+    #imgs = torch.cat((imgs,aug_imgs1), 0)
+    #y = torch.cat((y, y), 0)
     print(imgs.size(), y.size())
     return imgs, y
-'''
+
 class TrainDataset(Dataset):
     def __init__(self, raw_imgs, raw_y):
         aug_imgs, aug_y = flipped_data(raw_imgs, raw_y)
-        imgs = raw_imgs#np.concatenate((raw_imgs, aug_imgs), axis=0)
+        imgs = np.concatenate((raw_imgs, aug_imgs), axis=0)
         self.imgs = torch.tensor(imgs).type(torch.FloatTensor)
-        #y = np.concatenate((raw_y, aug_y), axis=0)
-        self.y = raw_y#torch.tensor(y).type(torch.LongTensor)
+        y = np.concatenate((raw_y, aug_y), axis=0)
+        self.y = torch.tensor(y).type(torch.LongTensor)
         self.transform = tf.Compose([
-            tf.ColorJitter(brightness=0.25, contrast=0.25, saturation=0.25, hue=0.25),
-            tf.RandomRotation(5),            
-            tf.RandomResizedCrop(48,scale=(0.95,1)),
+            tf.ToPILImage(),
+            tf.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+            tf.RandomRotation(20),            
+            tf.RandomResizedCrop(48,scale=(0.8,1)),
             tf.ToTensor()
         ])
     def __len__(self):
         return len(self.imgs)
 
     def __getitem__(self, idx):
-
-        PIL_img = tf.ToPILImage()(self.imgs[idx])
         #return self.imgs[idx], self.y[idx]
-        return self.transform(PIL_img).type(torch.FloatTensor), self.y[idx]
-'''
+        return self.transform(self.imgs[idx]).type(torch.FloatTensor), self.y[idx]
+
 if __name__ == "__main__":
     raw_imgs, raw_y = parse_csv(sys.argv[1])
     
-    num_val_data = raw_imgs.shape[0] // 20
+    num_val_data = raw_imgs.shape[0] // 6
     val_imgs = raw_imgs[:num_val_data,:,:]
     val_y = raw_y[:num_val_data]
 
@@ -97,7 +99,7 @@ if __name__ == "__main__":
     a = train_imgs.shape[0]
     print(train_imgs.shape[0], train_y.shape[0])
     print('before augmentation train_image:', train_imgs.shape)
-    train_imgs, train_y = augment_data(train_imgs, train_y)
+    #train_imgs, train_y = augment_data(train_imgs, train_y)
     print('after augmentation train_image:',train_imgs.shape)
     '''
     plt.imshow(train_imgs[0].squeeze(0).numpy() * 255, cmap='gray')
@@ -115,12 +117,12 @@ if __name__ == "__main__":
     quit()
     '''
 
-    training_set = Data.TensorDataset(train_imgs, train_y)#Data.TensorDataset(train_imgs[num_val_data:,:,:], train_y[num_val_data:])
+    training_set = TrainDataset(train_imgs, train_y)
     val_set = Data.TensorDataset(
         torch.tensor(val_imgs).type(torch.FloatTensor), 
         torch.tensor(val_y).type(torch.LongTensor))
     train_loader = DataLoader(
-        training_set, batch_size=BATCH_SIZE, shuffle=True)
+        training_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_set, batch_size=BATCH_SIZE, shuffle=False)
     
     
@@ -129,7 +131,12 @@ if __name__ == "__main__":
     # train
     device = torch.device('cuda')
     model = Model.MyCNN()
-    model.load_state_dict(torch.load('model_params.pkl'))
+    try:
+        model.load_state_dict(torch.load('model_params.pkl'))
+        print('use exist parameters')
+    except:
+        print('new model, no exist parameters')
+        pass
     model.to(device)
     optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
     loss_func = nn.CrossEntropyLoss()
@@ -137,7 +144,7 @@ if __name__ == "__main__":
     print('start training...')
     model.train()
 
-
+    high_val_acc = 0.67
     for epoch in range(EPOCH):
         train_loss, train_acc = [], []
         torch.cuda.empty_cache()
@@ -157,7 +164,7 @@ if __name__ == "__main__":
             acc = np.mean((target_cuda == predict).cpu().numpy())
             train_acc.append(acc)
             train_loss.append(loss.item())
-        
+        acc = np.mean(train_acc)
         val_acc = 0
         if num_val_data > 0:
             model.eval()
@@ -168,18 +175,19 @@ if __name__ == "__main__":
                 predict = torch.max(output, 1)[1]
                 val_acc += np.sum((target_cuda == predict).cpu().numpy())
             val_acc /= val_set.__len__()
+            if val_acc > high_val_acc:
+                high_val_acc = val_acc
+                torch.save(model.state_dict(), 'model_params.pkl')
+                print('saved new parameters')
             model.train()
 
-        print("Epoch: {}, Loss: {:.4f}, Acc: {:.4f}, Val Acc: {:.4f}"\
-            .format(epoch + 1,
-             np.mean(train_loss), np.mean(train_acc), val_acc
-             ))
+        print("Epoch: {}| Loss: {:.4f}| Acc: {:.4f}| Val Acc: {:.4f}"\
+            .format(epoch + 1, np.mean(train_loss), acc, val_acc))
     
     model.eval()
     # save parameters
     # torch.save(model, 'model.pkl') # entire net
     torch.save(model.state_dict(), 'model_params.pkl') # parameters
-    # validation
     # test
     output_file = open(sys.argv[3], 'w')
     output_file.write("id,label\n")
